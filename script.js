8045403839884822d1f231238200bdc592b13dc0
toezo <81764066+toezo@users.noreply.github.com>
2021-04-11 20:13:46 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-(function () {
+( function (){
+   var names=["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+   for(i=0;i<names.length;i++)
+   {
+       if(names[i].charAt(0).toLowerCase() === 'j')
+       {
+           Bye(names[i])
+       }
+       else{
+           Hello(names[i])
+       }
+   }
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-for (var i = 0; i < names.length; i++) {
-
-  var firstLetter = names[i].charAt(0).toLowerCase();
-  if (firstLetter === 'j') {
-    byeSpeaker.speak(names[i]);
-  } else {
-    helloSpeaker.speak(names[i]);
-  }
-}
 })();
 
